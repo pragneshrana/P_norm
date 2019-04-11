a943ee5fcbdaacd6cc06c59ac0bb9edeb44d506c
Pragnesh <35269274+pragneshrana@users.noreply.github.com>
2019-04-11 19:43:25 +0530

    code_uploaded

---
+++ p_norm.py
@@ -0,0 +1,39 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import math
+
+#We are assuming norm value is 1 and try to viosulize it 
+norm_value = 1
+
+#x values
+start = -1
+stop = 1
+steps = 10000
+p = 2
+x1 = np.linspace(start,stop,steps)
+
+def p_norm(x1,p):
+    x2 = []
+    negative_x2  = [] #as norm is 
+    for i in range(len(x1)):
+        temp_x2 =np.power(1-np.power(abs(x1[i]),p),1/p)
+        x2.append(temp_x2)
+        negative_x2.append(-temp_x2)
+
+    x2 = x2 + negative_x2 #aDDING NEGATIVE VALUES IN THE END  
+    x1 = np.append(x1,x1) #Settlement for plotting
+
+    plt.scatter(x1,x2)
+
+p_norm(x1,1)
+p_norm(x1,2)
+p_norm(x1,5)
+p_norm(x1,50)
+p_norm(x1,500)
+
+
+plt.show()
+
+
+
+     
